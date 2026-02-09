948573322b516240977e2910398d54aafcf10245
Christian P <113365748+cperona@users.noreply.github.com>
2026-02-09 14:26:56 +0100

    feat: add Editor class representing a newspaper editor

---
+++ src/main/java/org/example/Editor.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Editor {
+    private String name;
+    private final String ID;
+    //"Static attribute": The same shared value for all redactors.
+    private static int salary;
+
+    public Editor(String name, String ID, int salary) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public static int getSalary() {
+        return salary;
+    }
+
+    public static void setSalary(int salary) {
+        Editor.salary = salary;
+    }
+}
